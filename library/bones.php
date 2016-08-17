6b6bd360094b04c1d0c82240dea4e49fbfb72325
Andres Vega <gydoar@hotmail.com>
2016-08-16 23:26:50 -0400

    Agregando mas cambios

--- library/bones.php
+++ library/bones.php
@@ -138,6 +138,7 @@ function bones_scripts_and_styles() {
 		  wp_enqueue_script( 'comment-reply' );
     }
 
+
 		//adding scripts file in the footer
 		wp_register_script( 'bones-js', get_stylesheet_directory_uri() . '/library/js/scripts.js', array( 'jquery' ), '', true );
 
---
+++ page-home.php
@@ -0,0 +1,208 @@
+<?php
+/*
+ Template Name: Home
+ *
+*/
+?>
+
+<?php get_header(); ?>
+
+
+<!-- Slider caption -->
+<div>
+	
+</div>
+
+<!-- Section 2 De actualidad -->
+<div class="container">
+	<div class="seven columns de__actualidad">
+		<div class="row">
+			<h3><a href="#">DE ACTUALIDAD</a></h3> <div class="line__actualidad__home"></div>
+		</div>
+
+		<div class="row de__actualidad__cajas">
+			<div class="six columns">
+				<div class="center">
+					<a href="#"><img width="212px" src="<?php bloginfo('stylesheet_directory' ); ?>/library/images/home/de_actualidad.png" alt=""></a>
+				</div>
+
+				<h2>Las guías son especialmente aplicables si el problema es molesto para el paciente...</h2>
+
+				<a class="boton_vermas boton2" href="#">Leer más ></a>
+
+				<div class="espacio__actualidad__cajas"><br></div>
+
+				<h2>Las guías son especialmente aplicables si el problema es molesto para el paciente...</h2>
+
+				<a class="boton_vermas boton2" href="#">Leer más ></a>
+			
+			</div>
+			<div class="six columns">
+				<div class="center">
+					<a href="#"><img width="212px" src="<?php bloginfo('stylesheet_directory' ); ?>/library/images/home/de_actualidad.png" alt=""></a>
+				</div>
+
+				<h2>Las guías son especialmente aplicables si el problema es molesto para el paciente...</h2>
+
+				<a class="boton_vermas boton2" href="#">Leer más ></a>
+
+				<div class="espacio__actualidad__cajas"><br></div>
+
+				<h2>Las guías son especialmente aplicables si el problema es molesto para el paciente...</h2>
+
+				<a class="boton_vermas boton2" href="#">Leer más ></a>
+			</div>
+		</div>
+
+		<!-- CENTROS DE PROFESIONES -->
+			<div class="twelve columns centros__profesionales">
+				<div class="twelve columns">
+					<h3><a href="#">CENTROS DE PROFESIONES</a></h3> <div class="line__centros_profesionales"></div>
+				</div>
+
+				<div class="row centros_profesionales_cajas">
+					<div class="six columns">
+						<div>
+							<h4>Enfermería</h4>
+
+							<h2>Guía 2015 de Tratamiento de la Migraña Aguda en Adultos de la AHS …</h2>
+							<a class="boton_vermas boton2" href="#">Leer más ></a>
+							<br><br>
+						</div>
+
+						<div>
+							<h4>Medicina</h4>
+
+							<h2>Guía 2015 de Tratamiento de la Migraña Aguda en Adultos de la AHS …</h2>
+							<a class="boton_vermas boton2" href="#">Leer más ></a>
+						</div>
+					</div>
+					<div class="six columns">
+						<div>
+							<h4>Odontología</h4>
+
+							<h2>Guía 2015 de Tratamiento de la Migraña Aguda en Adultos de la AHS …</h2>
+							<a class="boton_vermas boton2" href="#">Leer más ></a>
+							<br><br>
+						</div>
+
+						<div>
+							<h4>Psicología</h4>
+
+							<h2>Guía 2015 de Tratamiento de la Migraña Aguda en Adultos de la AHS …</h2>
+							<a class="boton_vermas boton2" href="#">Leer más ></a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- BUSCANDO BIENESTAR -->
+			<div class="twelve columns centros__profesionales buscando_bienestar">
+				<div class="twelve columns">
+					<h3><a href="#">BUSCANDO BIENESTAR</a></h3> <div class="line__centros_profesionales"></div>
+				</div>
+
+				<div class="row centros_profesionales_cajas">
+					<div class="six columns">
+						<div>
+							<h4>Salud Femenina</h4>
+
+							<h2>Guía 2015 de Tratamiento de la Migraña Aguda en Adultos de la AHS …</h2>
+							<a class="boton_vermas boton2" href="#">Leer más ></a>
+							<br>
+						</div>
+						<div class="line__buscando__bienestar"></div>
+						<div>
+
+							<h2>Guía 2015 de Tratamiento de la Migraña Aguda en Adultos de la AHS …</h2>
+							<a class="boton_vermas boton2" href="#">Leer más ></a>
+						</div>
+					</div>
+					<div class="six columns">
+						<div>
+							<h4>Salud en Niños</h4>
+
+							<h2>Guía 2015 de Tratamiento de la Migraña Aguda en Adultos de la AHS …</h2>
+							<a class="boton_vermas boton2" href="#">Leer más ></a>
+							<br>
+						</div>
+						<div class="line__buscando__bienestar"></div>
+						<div>
+
+							<h2>Guía 2015 de Tratamiento de la Migraña Aguda en Adultos de la AHS …</h2>
+							<a class="boton_vermas boton2" href="#">Leer más ></a>
+						</div>
+					</div>
+				</div>
+			</div>
+	</div>
+		
+
+	<!-- Sidebar -->
+	<!-- lo destacado -->
+	<div class="five columns lo__destacado">
+		<div class="row">
+			<h3><a href="#">LO DESTACADO</a></h3> <div class="line__destacado__home"></div>
+		</div>
+
+		<div class="row">
+			<ul>
+				<li><a href="#">¿Qué es la diabetes?</a></li>
+				<li><a href="#">¿Pueden los niños sufrir de diabetes tipo 2?</a></li>
+				<li><a href="#">¿Qué es la diabetes?</a></li>
+				<li><a href="#">¿Qué es la diabetes?</a></li>
+				<li><a href="#">¿Pueden los niños sufrir de diabetes tipo 2?</a></li>
+				<li><a href="#">¿Qué es la diabetes?</a></li>
+				<li><a href="#">¿Qué es la diabetes?</a></li>
+				<li><a href="#">¿Pueden los niños sufrir de diabetes tipo 2?</a></li>
+				<li><a href="#">¿Qué es la diabetes?</a></li>
+				<li><a href="#">¿Pueden los niños sufrir de diabetes tipo 2?</a></li>
+			</ul>
+		</div>
+
+		<!-- Para el paciente -->
+		<div class="twelve columns para_el_paciente">
+			<h3><a href="#">Para el Paciente</a></h3>
+
+			<div class="row para__el__paciente__cajas">
+				<div class="four columns">
+					<img class="twelve columns" src="<?php bloginfo('stylesheet_directory' ); ?>/library/images/home/para_paciente.png" alt="">
+				</div>
+				<div class="eight columns">
+
+					<h2>Rebajar consumo de carnes rojas disminuye riesgo ...</h2>
+
+					<p>El consumo de carnes rojas, sobre todo procesadas, se ha asociado de manera consistente a aumento en el riesgo de dia...</p>
+					
+					<a class="boton_vermas boton2" href="#">Leer más ></a>
+				</div>
+			</div>
+			<div class="line__sidebar__paciente"></div>
+			<div class="row para__el__paciente__cajas">
+				<div class="four columns">
+					<img class="twelve columns" src="<?php bloginfo('stylesheet_directory' ); ?>/library/images/home/para_paciente-2.png" alt="">
+				</div>
+				<div class="eight columns">
+
+					<h2>Rebajar consumo de carnes rojas disminuye riesgo ...</h2>
+
+					<p>El consumo de carnes rojas, sobre todo procesadas, se ha asociado de manera consistente a aumento en el riesgo de dia...</p>
+					
+					<a class="boton_vermas boton2" href="#">Leer más ></a>
+				</div>
+			</div>
+		</div>
+
+		<!-- Especiales Salud hoy -->
+		<div class="twelve colums especiales__salud__hoy">
+			<div class="row columns">
+				<h3><a href="#">Especiales SALUD HOY</a></h3>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+
+
+<?php get_footer(); ?>
